284a7cad8b44d23596a58ffc4557e6a1d6de645e
Tố Uyên <dungbawngha19102004@gmail.com>
2024-08-19 06:32:11 +0700

    fourth commit

--- obj/Debug/net8.0/Handmade_Dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Handmade_Dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Handmade_Dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c35e2876397e2a1be0adb7cbe6bc9d6877b2c58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee012a8b2fa6243b5e7239792da378b7b5fba98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Handmade_Dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Handmade_Dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
